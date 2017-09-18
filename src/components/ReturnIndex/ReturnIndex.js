d524da2136e8d99f2535c2a4ccbc719ce923f50f
LDQ-first <178848890@qq.com>
2017-09-19 00:30:31 +0800

    'modify'

--- src/components/ReturnIndex/ReturnIndex.js
+++ src/components/ReturnIndex/ReturnIndex.js
@@ -3,12 +3,20 @@ import ReturnIndexDiv from '../../styled/ReturnIndex.js'
 import {Btn} from '../../styled/ReturnIndex.js'
 import FlatButton from 'material-ui/FlatButton'
 
+
+
 export default class ReturnIndex extends Component {
+    
+
     render() {
+        
+    const indexUrl = process.env.NODE_ENV === 'production' 
+    ? '/react-allfamily/dist/'
+    : '/'
         return (
             <ReturnIndexDiv>
                 <FlatButton
-                href="/"
+                href={indexUrl}
                 label="Eyepetizer"
                 labelStyle = { Btn }
                 />
