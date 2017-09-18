c0c8196aa6ea19d00f6054a63664c16c082ea39b
LDQ-first <178848890@qq.com>
2017-09-19 00:33:30 +0800

    'modify'

--- src/components/ReturnIndex/ReturnIndex.js
+++ src/components/ReturnIndex/ReturnIndex.js
@@ -11,7 +11,7 @@ export default class ReturnIndex extends Component {
     render() {
         
     const indexUrl = process.env.NODE_ENV === 'production' 
-    ? '/react-allfamily/dist/'
+    ? '/react-eyepetizer/dist/'
     : '/'
         return (
             <ReturnIndexDiv>
