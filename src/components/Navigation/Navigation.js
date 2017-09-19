9463c452bf41cb838b4623f95d39b61914a5d334
LDQ-first <178848890@qq.com>
2017-09-20 01:43:47 +0800

    'modify'

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react'
 import NavigationDiv from '../../styled/Navigation.js'
 import IconButton from 'material-ui/IconButton'
 import ActionSearch from 'material-ui/svg-icons/action/search'
-import {searchArea} from '../../router/link.js'
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
 import Paper from 'material-ui/Paper'
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on'
+import ActionChangeHistory from 'material-ui/svg-icons/action/change-history'
+import ToggleRadioButtonUnchecked from 'material-ui/svg-icons/toggle/radio-button-unchecked'
+import ToggleStar from 'material-ui/svg-icons/toggle/star'
+import {home, find, follow} from '../../router/link.js'
+
 
 
 export default class Navigation extends Component {
     static get propTypes() { 
         return { 
            _this: PropTypes.object,
-           
+           history: PropTypes.object
         }
     }
 
@@ -25,10 +28,17 @@ export default class Navigation extends Component {
 
    
 
-    select (index) { 
+
+     switchRoute(path) {
+        const {history} = this.props
+        history.push(path)
+    }
+
+    select (index, path)  {
         this.setState({
             selectedIndex: index
-       })
+        })
+        this.switchRoute(path)
     }
 
 
@@ -42,21 +52,21 @@ export default class Navigation extends Component {
             <NavigationDiv>
                 <Paper zDepth={1}>
                     <BottomNavigation selectedIndex={selectedIndex}>
-                    <BottomNavigationItem
-                        label="首页"
-                        icon={}
-                        onClick={() => this.select(0)}
-                    />
-                    <BottomNavigationItem
-                        label="发现"
-                        icon={}
-                        onClick={() => this.select(1)}
-                    />
-                    <BottomNavigationItem
-                        label="关注"
-                        icon={}
-                        onClick={() => this.select(2)}
-                    />
+                        <BottomNavigationItem
+                            label="首页"
+                            icon={<ActionChangeHistory />}
+                            onClick={() => this.select(0, `${home}`)}
+                        />
+                        <BottomNavigationItem
+                            label="发现"
+                            icon={<ToggleRadioButtonUnchecked />}
+                            onClick={() => this.select(1, `${find}`)}
+                        />
+                        <BottomNavigationItem
+                            label="关注"
+                            icon={<ToggleStar />}
+                            onClick={() => this.select(2, `${follow}`)}
+                        />
                     </BottomNavigation>
                 </Paper>
             </NavigationDiv>
